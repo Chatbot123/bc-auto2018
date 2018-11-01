709db52d66b74fb41275cdf3f282540e7ef19709
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-11-01 16:39:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,13 +13,12 @@ if($method == 'POST')
     		);
     		curl_setopt_array( $ch, $options );
 		$json = curl_exec( $ch );
-	//echo $json;
 		$someobj = json_decode($json,true);
-		echo $someobj->fulfillmentText;
-		//	$speech_data = $file->results->result->fulfillment->speech;
-       			// $nexttick=time()+10;
-		// $active=false;	
-    	
+		$speech = $someobj->fulfillmentText;
+		
+    	$response = new \stdClass();
+    	$response->fulfillmentText = $speech;
+    	$response->source = "webhook";
 	}
 else
 {
