66c11c417e6225f7ad0470e7eb44871b7e843f82
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-11-01 16:16:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ $method = $_SERVER['REQUEST_METHOD'];
 //process only when method id post
 if($method == 'POST')
 {
-	//$requestBody = file_get_contents('php://input');
+//$requestBody = file_get_contents('php://input');
 	//$json = json_decode($requestBody);
-	$json_url = "https://dev60887.service-now.com/api/289816/incidentcreate";
+	---$json_url = "https://dev60887.service-now.com/api/289816/incidentcreate";
 		$username    = "admin";
     		$password    = "Avik.17.jan";
 		$ch      = curl_init( $json_url );
@@ -13,7 +13,7 @@ if($method == 'POST')
         	CURLOPT_SSL_VERIFYPEER => false,
         	CURLOPT_RETURNTRANSFER => true,
         	CURLOPT_USERPWD        => "{$username}:{$password}",
-        	CURLOPT_HTTPHEADER     => array( "Accept: application/json" ),
+        	CURLOPT_HTTPHEADER     => array( "Accept: application/json" )
     		);
     		curl_setopt_array( $ch, $options );
 		$json = curl_exec( $ch );
@@ -29,5 +29,10 @@ else
 {
 	echo "Method not allowed";
 }
+  
+	
+	
+}
+
 
 ?>
