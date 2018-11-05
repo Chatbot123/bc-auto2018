5c0aec6fd05d1078d210c96c568f3d72a49ba9c1
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-11-05 11:42:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -55,6 +55,60 @@ if($method == 'POST')
 		
 
 	}
+	if($json->queryResult->intent->displayName=='Get_Status_ticket')
+	{
+		if(isset($json->queryResult->queryText))
+		{ $sh_desc = $json->queryResult->queryText; }
+
+		if(isset($json->queryResult->parameters->Raisedate))
+		{ $Raisedate = $json->queryResult->parameters->Raisedate; }
+		
+		if(isset($json->queryResult->parameters->Ticketno))
+		{ $Ticketno = $json->queryResult->parameters->Ticketno; }
+		
+		$Raisedate = substr($Raisedate, 0, 10);
+		
+		
+		$sh_desc = strtolower($sh_desc);
+		//$sh_desc = "Testing";
+		//$name = "someone";
+		$instance = "dev60887";
+		$username = "admin";
+		$password = "Avik.17.jan";
+		$table = "incident";
+		//$jsonobj = "{\"short_description\":$sh_desc,\"priority\":\"1\",\"Caller_id\":\"someone\"}";
+		//$jsonobj = array('short_description' => $sh_desc);
+             	//$jsonobj = json_encode($jsonobj);	
+
+		//$jsonobj = "{\"short_description\":$sh_desc,\"priority\":\"1\",\"Caller_id\":$name}";
+		$query = "https://$instance.service-now.com/$table.do?JSONv2&sysparm_action=getRecords&sysparm_query=numberENDSWITH".$Ticketno."^sys_created_onSTARTSWITH".$Raisedate;
+		$curl = curl_init($query);
+		curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($curl, CURLOPT_USERPWD, "$username:$password");
+		curl_setopt($curl, CURLOPT_VERBOSE, 1);
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+
+		/*if($jsonobj)
+		{
+			    curl_setopt($curl, CURLOPT_POST, true);
+			    curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+			    curl_setopt($curl, CURLOPT_POSTFIELDS, $jsonobj);
+		}*/
+		$response = curl_exec($curl);
+		curl_close($curl);
+		$jsonoutput = json_decode($response);
+		$speech= $jsonoutput;
+		//$incident_no =  $jsonoutput->records[0]->number;
+		
+		//$speech = "Thanks ".$name."! Incident Created Successfully for issue " . $sh_desc . " and your incident number is " . $incident_no;
+		//echo $speech;
+		
+
+	}
 		$res = new \stdClass();
 		$res->fulfillmentText = $speech;
 		$res->source = "webhook";
