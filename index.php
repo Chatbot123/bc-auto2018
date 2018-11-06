b88f0e389bcb5882ef6a0b20769ef843387ab39e
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-11-06 11:06:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -123,6 +123,35 @@ if($method == 'POST')
 		
 
 	}
+	
+	//--------------------
+	if($json->queryResult->intent->displayName=='SENDMAIL')
+	{
+		if(isset($json->queryResult->parameters->to_email))
+		{ $to_email = $json->queryResult->parameters->to_email; }
+	//require 'PHPMailer/class.phpmailer.php';
+	$mail = new PHPMailer(); // create a new object
+	$mail->IsSMTP(); // enable SMTP
+	$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+	$mail->SMTPAuth = true; // authentication enabled
+	$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+	$mail->Host = "smtp.gmail.com";
+	$mail->Port = 465; // or 587
+	$mail->IsHTML(true);
+	$mail->Username = "intelligentmachine2018@gmail.com";
+	$mail->Password = "Centurylink2018";
+	$mail->SetFrom($to_email);
+	$mail->Subject = "Test";
+	$mail->Body = "hello";
+	$mail->AddAddress($to_email);
+
+	 if(!$mail->Send()) {
+	    $speech= "Mailer Error: " . $mail->ErrorInfo;
+	 } else {
+	    $speech= "Message has been sent";
+	 }
+	}
+	//--------------------
 		$res = new \stdClass();
 		$res->fulfillmentText = $speech;
 		$res->source = "webhook";
