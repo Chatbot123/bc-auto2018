000ff6ae92e1af268831847a2185364465b1db29
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2018-11-20 14:16:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -202,7 +202,61 @@ if($method == 'POST')
 		
 		
 	}
-	
+	if($json->queryResult->intent->displayName=='SCT_UnlockSapAccount - no - yes')
+	{
+		
+		if(isset($json->queryResult->parameters->line_manager))
+		{ $line_manager = $json->queryResult->parameters->line_manager; }
+		
+		
+			
+		$instance = "dev60887";
+		$username = "admin";
+		$password = "Avik.17.jan";
+		$table = "incident";
+		
+		$query = "https://$instance.service-now.com/api/sn_sc/v1/servicecatalog/items/d292507adb3123002e6ff36f29961911/add_to_cart";
+		$curl = curl_init($query);
+		curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($curl, CURLOPT_USERPWD, "$username:$password");
+		curl_setopt($curl, CURLOPT_VERBOSE, 1);
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+		if($jsonobj)
+		{
+			    curl_setopt($curl, CURLOPT_POST, true);
+			    curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+			    curl_setopt($curl, CURLOPT_POSTFIELDS, "{ \"sysparm_quantity\": \"1\",\"variables\":{ }}");
+		}
+		$response = curl_exec($curl);
+		curl_close($curl);
+	//	$jsonoutput = json_decode($response);
+	//	$item_name =  $jsonoutput->result->items[0]->item_name;
+		
+		
+		$query = "https://dev60887.service-now.com/api/sn_sc/v1/servicecatalog/cart/submit_order";
+		$curl = curl_init($query);
+		curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($curl, CURLOPT_USERPWD, "$username:$password");
+		curl_setopt($curl, CURLOPT_VERBOSE, 1);
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+
+		$response = curl_exec($curl);
+		curl_close($curl);
+		$jsonoutput = json_decode($response);
+		$request_num =  $jsonoutput->result->request_number;
+		$speech = "Your Request number is ".$request_num." Please attach approval of your Line Manager to the ticket, so that your account will be unlocked.";
+ 
+		
+
+	}
 	//--------------------
 	if($json->queryResult->intent->displayName=='SENDMAIL')
 	{
